bb9c1e8ea30a1c661455f2f7526ab7679a2031f0
jhennies <julianhennies@hotmail.de>
2026-08-04 15:53:05 +0200

    updating hydra workflow for new matrix api

--- squirrel/workflows/hydra.py
+++ squirrel/workflows/hydra.py
@@ -14,16 +14,19 @@ def get_acquisition_transforms_workflow(dirpath: str, out_filepath: str = None, 
 
     # Convert to an affine sequence
     from squirrel.library.affine_matrices import AffineMatrix, AffineStack
-    transforms = AffineStack(stack=[AffineMatrix(translation=-x) for x in origins], is_sequenced=True)
+    # transforms = AffineStack(stack=[AffineMatrix(translation=-x) for x in origins], is_sequenced=True)
+    transforms = AffineStack.from_array(matrices=[AffineMatrix.from_translation(-x) for x in origins], sequenced=True)
 
     # Apply autopadding 
-    from squirrel.library.image import apply_auto_pad
+    # from squirrel.library.image import apply_auto_pad
     stack_bounds = np.concatenate((np.zeros(shapes.shape), shapes), axis=1)
-    transforms, stack_shape = apply_auto_pad(
-        transforms, [len(transforms), 0, 0], stack_bounds, extra_padding=16
-    )
-    transforms.set_meta('shape', stack_shape)
-    transforms.set_meta('bounds', stack_bounds)
+    # transforms, stack_shape = apply_auto_pad(
+    #     transforms, [len(transforms), 0, 0], stack_bounds, extra_padding=16
+    # )
+    # transforms.set_meta('shape', stack_shape)
+    # transforms.set_meta('bounds', stack_bounds)
+    transforms.set_metadata('bounds', stack_bounds)
+    transforms, _ = transforms.auto_pad(extra_padding=16)
 
     if out_filepath is not None:
         transforms.to_file(out_filepath)
